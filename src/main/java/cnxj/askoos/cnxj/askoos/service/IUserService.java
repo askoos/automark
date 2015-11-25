28f13f9a2e24110d09ef1c89bca6aa0fd3f26985
askoos <askoos2032@sina.com>
2015-11-25 18:54:15 +0800

    第一次提交（初始提交）

---
+++ src/main/java/cnxj/askoos/cnxj/askoos/service/IUserService.java
@@ -0,0 +1,18 @@
+package cnxj.askoos.cnxj.askoos.service;
+
+import cnxj.askoos.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Askoos on 2015/11/25.
+ */
+public interface IUserService {
+
+    public void add(User user);
+    public User findById(Long id);
+    public List<User> findAll();
+    public User update(User user);
+    public void delete(Long id);
+
+}
