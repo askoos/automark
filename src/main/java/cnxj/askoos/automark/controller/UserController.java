672fd8eab6b17afbdad0c32a9b43384278da5562
askoos <askoos2032@sina.com>
2015-11-25 16:10:06 +0800

    第一次提交（初始提交）

--- src/main/java/cnxj/askoos/automark/controller/UserController.java
+++ src/main/java/cnxj/askoos/automark/controller/UserController.java
@@ -4,7 +4,6 @@ import cnxj.askoos.automark.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,10 +30,9 @@ public class UserController {
     }
 
     @RequestMapping("/showUser")
-    public
     @ResponseBody
-    User showUser() {
-        return new User(11,"Askoos", 36);
+    public User showUser() {
+        return new User(11L, "Askoos", 36);
     }
 
     public static void main(String[] args) {
