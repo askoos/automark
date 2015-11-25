f97e16d3eaeaea1509898b68ce634f6a573f74a2
askoos <askoos2032@sina.com>
2015-11-25 19:08:26 +0800

    第一次提交（初始提交）

--- src/main/java/cnxj/askoos/service/impl/UserServiceImpl.java
+++ src/main/java/cnxj/askoos/service/impl/UserServiceImpl.java
@@ -18,13 +18,13 @@ public class UserServiceImpl  implements IUserService {
     /**
      * 缓存的key
      */
-    public static final String THING_ALL_KEY = "\"user_all\"";
+    public static final String USER_ALL_KEY = "\"user_all\"";
     /**
      * value属性表示使用哪个缓存策略，缓存策略在ehcache.xml
      */
-    public static final String DEMO_CACHE_NAME = "allShare";
+    public static final String USER_CACHE_NAME = "allShare";
 
-    @CacheEvict(value = DEMO_CACHE_NAME, key = THING_ALL_KEY)
+    @CacheEvict(value = USER_CACHE_NAME, key = USER_ALL_KEY)
     @Override
     public void add(User user) {
         Long id = getNextId();
@@ -34,14 +34,14 @@ public class UserServiceImpl  implements IUserService {
         data.put(id, user);
     }
 
-    @Cacheable(value = DEMO_CACHE_NAME, key = "#user.getId()+'thing'")
+    @Cacheable(value = USER_CACHE_NAME, key = "#user.getId()+'thing'")
     @Override
     public User findById(Long id) {
         System.err.println("没有走缓存！" + id);
         return data.get(id);
     }
 
-    @Cacheable(value = DEMO_CACHE_NAME, key = THING_ALL_KEY)
+    @Cacheable(value = USER_CACHE_NAME, key = USER_ALL_KEY)
     @Override
     public List<User> findAll() {
         return Lists.newArrayList(data.values());
@@ -49,15 +49,15 @@ public class UserServiceImpl  implements IUserService {
 
 
     @Override
-    @CachePut(value = DEMO_CACHE_NAME, key = "#user.getId()+'User'")
-    @CacheEvict(value = DEMO_CACHE_NAME, key = THING_ALL_KEY)
+    @CachePut(value = USER_CACHE_NAME, key = "#user.getId()+'User'")
+    @CacheEvict(value = USER_CACHE_NAME, key = USER_ALL_KEY)
     public User update(User user) {
         System.out.println(user);
         data.put(user.getId(), user);
         return user;
     }
 
-    @CacheEvict(value = DEMO_CACHE_NAME)
+    @CacheEvict(value = USER_CACHE_NAME)
     @Override
     public void delete(Long id) {
         data.remove(id);
